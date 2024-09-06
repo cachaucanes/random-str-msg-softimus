8732829be46be712414dd50c9f99d7b79c41ab0c
Carlos Chaucanes - @Softimus <cachaucanes23@gmail.com>
2024-09-06 17:26:40 -0500

    [ADD] COMMIT ALL THE FILES!

---
+++ bin/global.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/* 
+    la linea de arriba Genera una instrucción para indicar que este 
+    archivo va a funcionar del lado de node
+ */
+
+let random = require('../src/index.js')
+
+random.funnyCommit()
